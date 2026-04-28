84baa09eeb26b0b756ea9f35a342fafa9e145bd4
Vaidehi1424 <146956389+Vaidehi1424@users.noreply.github.com>
2026-04-28 13:30:01 +0530

    Update SecurityConfig.java

--- src/main/java/com/smarttraffic/security/SecurityConfig.java
+++ src/main/java/com/smarttraffic/security/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.smarttraffic.security;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
