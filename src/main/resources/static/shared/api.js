70d03ec4b7bac805fead50136c73f0f62ca6a162
Vaidehi1424 <146956389+Vaidehi1424@users.noreply.github.com>
2026-04-28 14:51:55 +0530

    Update api.js

--- src/main/resources/static/shared/api.js
+++ src/main/resources/static/shared/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = "https://smart-traffic-system-y862.onrender.com";
 
 // STORAGE
 function saveToken(token) { localStorage.setItem("token", token); }
