36be2a9591d04440f770621d34f8f8678cf5fa69
Vaidehi1424 <146956389+Vaidehi1424@users.noreply.github.com>
2026-04-28 15:54:30 +0530

    Update User.java

--- src/main/java/com/smarttraffic/model/User.java
+++ src/main/java/com/smarttraffic/model/User.java
@@ -7,8 +7,8 @@ import jakarta.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+@GeneratedValue(strategy = GenerationType.AUTO)
+private Long id;
 
     @Column(unique = true, nullable = false)
     private String username;
